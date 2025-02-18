b21dc7eb68c5fe33ccdeebc00f72a8dcd1e868d9
MabelYT <mabeliscool1234@gmail.com>
2025-02-18 20:36:54 +0000

    Pass Command Output To DOS/32 Exec Via Shared Clipboard Device

--- Runtime/src/Main.cc
+++ Runtime/src/Main.cc
@@ -1,12 +1,27 @@
 #include <conio.h>
+#include <filesystem>
+#include <fstream>
 #include <string>
 
-void HostCMD(std::string CommandLine) {
-    system((std::string("START ") + CommandLine).c_str());
+using std::string;
+
+void HostCMD(string CommandLine) {
+    system((string("START ") + CommandLine + " /MIN").c_str());
+}
+
+string ReadClip() {
+    std::fstream File("CLIP$");
+    string Content {
+        std::istreambuf_iterator<char>(File), std::istreambuf_iterator<char>()
+    };
+    return Content.substr(0, Content.length() - 1);
 }
 
 int main() {
-    cprintf("Launching Host Process (calc.exe)...\r\n");
-    HostCMD("calc.exe");
+    HostCMD("\"CMD /C HOSTNAME|CLIP\"");
+    string HostName = ReadClip();
+    HostCMD("\"CMD /C ECHO %USERNAME%|CLIP\"");
+    string UserName = ReadClip();
+    cprintf((string("Hello ") + UserName + ", On Machine " + HostName + "!\r\n").c_str());
     return 0;
 }
